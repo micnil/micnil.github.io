59d70e387b05b7571d0760ac60fdecda20fa1a56
micnil <michaelnilsson90@hotmail.com>
2015-10-22 20:40:59 +0200

    updated about page. fixed with some padding issues

--- transitions.js
+++ transitions.js
@@ -14,36 +14,6 @@ function load(file) {
 	xhr.send();
 };
 
-/*function onBoxClickEvent(e){
-	var more=e.target;
-	if(more.hasChildNodes()){
-
-		var nodes = more.childNodes;
-		for (var i = 0; i < nodes.length; i++) {
-			if(nodes.item(i).className == "more"){
-				more=nodes.item(i);
-				break;
-			}
-		}
-	}
-	if(more.parentNode.className=="more"){
-		more = more.parentNode;
-	}else{
-		while (more.className != "more")
-			more = more.nextSibling;
-	}
-
-
-
-	if(more.style.display=="inline"){
-		more.style.display = "none";
-	}
-	else{
-		more.style.display = "inline";
-	}
-};*/
-
-
 function onBoxClickEvent(e){
 	$(e.currentTarget).children('.more').toggleClass('hidden');
 };

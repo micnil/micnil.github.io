55e1c283cc1ba4877a299f453a5bfac8ea89d0f1
Michael Nilsson <michaelnilsson90@hotmail.com>
2019-03-06 21:28:58 +0100

    Replace svg introduction with html code block

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import Layout from '../components/Layout';
 import SEO from '../components/Seo';
-import IntroCommitSvg from '../images/git-commit-intro.svg';
 import { HTMLContent } from '../components/Content'
+import CodeBlock from "../components/CodeBlock";
 import Img from 'gatsby-image';
 import { graphql } from 'gatsby';
 import {
@@ -66,13 +66,13 @@ const SocialLinks = styled.div`
   }
 `;
 
-const IntroImage = styled.img`
-  @media only screen and (min-width: 768px) {
-    max-width: 768px;
-  }
-  margin: auto;
-  margin-bottom: 1rem;
-  display: block;
+
+const YellowText = styled.span`
+  color: rgb(255, 204, 0);
+`;
+
+const GreyText = styled.span`
+  color: rgb(174, 174, 170);
 `;
 
 const ProfileContainerDesktop = styled.div`
@@ -105,7 +105,27 @@ const IndexPage = ({ data }) => {
     <Layout>
       <SEO title="Home" keywords={['software', 'developer']} />
       <MobileOnlyGreeting><h1>Hello, I'm Michael</h1></MobileOnlyGreeting>
-      <IntroImage src={IntroCommitSvg} alt="Introduction to Michael Nilsson" />
+      <CodeBlock>
+        <YellowText>></YellowText><span>{` git log -1
+`}</span><GreyText>commit daff16378c079fb52c1bd27dc6f630988b0d222a</GreyText><span>{`
+Author: Michael Nilsson <`}</span><YellowText><span>
+{firstname}
+<span style={{ display: 'none' }} />
+&#64;{lastname}
+<span>{null}</span>&#46;dev
+</span></YellowText><span>{`>
+Date:   Sun Mar 3 19:20:58 2019 +0200
+
+    `}</span><YellowText>[#2157] Add introduction to Michael Nilsson</YellowText><span>{`
+
+    Explain that Michael is a Swedish software developer, that enjoys
+    solving interesting problems both professionally and as a hobby.
+    Promote his interests in new technology, and that he is driven by
+    building innovative solutions. Mention that he has a strong 
+    background in `}</span><YellowText>web technologies</YellowText><span>{`, `}</span><YellowText>.NET</YellowText><span>{` and `}</span><YellowText>computer graphics</YellowText><span>{`. 
+
+    Also point out that he has a `}</span><YellowText>Master of Science in Media Technology</YellowText><span>{`.`}</span>
+      </CodeBlock>
       <ContactContainer>
         <ProfileContainerDesktop>
           <RoundImg fluid={data.photo.childImageSharp.fluid} />

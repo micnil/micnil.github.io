2ae6493af0ea41d948695a5d2acb29b989732c4e
Michael Nilsson <michael@nilsson.dev>
2020-07-12 22:12:43 +0200

    Update failing tests and a new test case

--- src/components/cv/Section.test.js
+++ src/components/cv/Section.test.js
@@ -6,20 +6,26 @@ import { SectionWithYear } from './Section';
 const entries = [
   {
     filePath: 'C:/test/file1.md',
-    start: 2016,
-    end: 2017,
+    start: '2016',
+    end: '2017',
     content: 'foo',
   },
   {
     filePath: 'C:/test/file2.md',
-    start: 2014,
+    start: '2014',
     content: 'bar',
   },
   {
     filePath: 'C:/test/file3.md',
-    start: 2016,
-    end: 2016,
+    start: '2016',
+    end: '2016',
     content: 'baz',
+  },
+  {
+    filePath: 'C:/test/file4.md',
+    start: '2016-04-07',
+    end: '2017-07-25',
+    content: 'qux',
   },
 ];
 
@@ -35,7 +41,7 @@ describe('SectionWithYear', () => {
     const { getAllByTestId } = render(
       <SectionWithYear section={data.section} entries={data.entries} />
     );
-    const contents = ['bar', 'foo', 'baz'];
+    const contents = ['bar', 'foo', 'baz', 'qux'];
     const renderedContents = getAllByTestId('content');
     renderedContents.forEach((contentNode, index) => {
       expect(contentNode.textContent).toBe(contents[index]);
@@ -47,7 +53,7 @@ describe('SectionWithYear', () => {
       <SectionWithYear section={data.section} entries={[entries[0]]} />
     );
     const pastRange = getByTestId('year');
-    expect(pastRange.textContent).toBe('2016 - 2017');
+    expect(pastRange.textContent).toBe('2016–2017');
   });
 
   it('displays current correctly', () => {
@@ -55,7 +61,7 @@ describe('SectionWithYear', () => {
       <SectionWithYear section={data.section} entries={[entries[1]]} />
     );
     const current = getByTestId('year');
-    expect(current.textContent).toBe('2014 - Present');
+    expect(current.textContent).toBe('2014–Present');
   });
 
   it('displays single year correctly', () => {
@@ -64,5 +70,12 @@ describe('SectionWithYear', () => {
     );
     const singleYear = getByTestId('year');
     expect(singleYear.textContent).toBe('2016');
+  });
+  it('More specific range if timestamp is more specific', () => {
+    const { getByTestId } = render(
+      <SectionWithYear section={data.section} entries={[entries[3]]} />
+    );
+    const singleYear = getByTestId('year');
+    expect(singleYear.textContent).toBe('Apr 2016–Jul 2017');
   });
 });
